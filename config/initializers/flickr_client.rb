ce319d0265e015411167b86a6a88c5bd34acf6a3
Doug Emery <emeryr@upenn.edu>
2016-01-24 20:59:08 -0500

    Make FlickrClient initializer work in development

--- config/initializers/flickr_client.rb
+++ config/initializers/flickr_client.rb
@@ -1,8 +1,16 @@
-Subpop::FlickrClient.setup do |config|
-  config.flickr_api_key       = ENV['SUBPOP_FLICKR_API_KEY']
-  config.flickr_shared_secret = ENV['SUBPOP_FLICKR_API_SECRET']
-  config.flickr_access_token  = ENV['SUBPOP_FLICKR_ACCESS_TOKEN']
-  config.flickr_access_secret = ENV['SUBPOP_FLICKR_ACCESS_SECRET']
-  config.flickr_userid        = ENV['SUBPOP_FLICKR_USERID']
-  config.flickr_username      = ENV['SUBPOP_FLICKR_USERNAME']
+module Subpop
+  class Application < Rails::Application
+    config.to_prepare do
+      FlickrClient.setup do |config|
+        config.flickr_api_key       = ENV['SUBPOP_FLICKR_API_KEY']
+        config.flickr_shared_secret = ENV['SUBPOP_FLICKR_API_SECRET']
+        config.flickr_access_token  = ENV['SUBPOP_FLICKR_ACCESS_TOKEN']
+        config.flickr_access_secret = ENV['SUBPOP_FLICKR_ACCESS_SECRET']
+        config.flickr_userid        = ENV['SUBPOP_FLICKR_USERID']
+        config.flickr_username      = ENV['SUBPOP_FLICKR_USERNAME']
+      end
+
+      FlickrClient.validate
+    end
+  end
 end
