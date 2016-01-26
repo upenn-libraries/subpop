685a9e8eba095fa8c3e3a48e99792774469c72b4
Doug Emery <emeryr@upenn.edu>
2016-01-26 15:03:15 -0500

    Prettify alerts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -19,4 +19,19 @@ module ApplicationHelper
     end
     link_to(name, '#', class: "add_fields", data: {id: id, fields: fields.gsub("\n", "")})
   end
+
+  def bootstrap_class_for(flash_type)
+    case flash_type
+    when "success"
+      "alert-success"   # Green
+    when "error"
+      "alert-danger"    # Red
+    when "alert"
+      "alert-warning"   # Yellow
+    when "notice"
+      "alert-info"      # Blue
+    else
+      flash_type.to_s
+    end
+  end
 end
