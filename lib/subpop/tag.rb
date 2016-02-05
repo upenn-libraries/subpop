055ffd5e2aeb49f5f66c29276804fd99609da72b
Doug Emery <emeryr@upenn.edu>
2016-02-05 10:18:29 -0500

    Model behavior of Flickr tag in own class

---
+++ lib/subpop/tag.rb
@@ -0,0 +1,29 @@
+module Subpop
+  class Tag
+    attr_reader :str
+
+    def initialize str
+      @str = str
+    end
+
+    def quote open='`', close='\''
+      open + str + close
+    end
+
+    def dquote
+      quote '"', '"'
+    end
+
+    def squote
+      quote "'", "'"
+    end
+
+    def normalize
+      str && str.downcase.gsub(/[^\p{Alnum}]+/, '')
+    end
+
+    def to_s
+      str
+    end
+  end
+end
---
+++ spec/subpop/tag_spec.rb
@@ -0,0 +1,45 @@
+require 'rails_helper'
+
+module Subpop
+  RSpec.describe Tag do
+    context "initialize" do
+      it "creates a Tag" do
+        expect {
+          Tag.new "tag string"
+          }.not_to raise_error
+      end
+    end
+
+    context "modification" do
+      it "quotes the string" do
+        expect(Tag.new("tag string").quote).to eq("`tag string'")
+      end
+
+      it "single quotes the string" do
+        expect(Tag.new("tag string").squote).to eq("'tag string'")
+      end
+
+      it "double quotes the string" do
+        expect(Tag.new("tag string").dquote).to eq('"tag string"')
+      end
+    end
+
+    context "normalize" do
+      it "removes spaces" do
+        expect(Tag.new("tag string").normalize).to eq("tagstring")
+      end
+
+      it "respects diacritics" do
+        expect(Tag.new("münchen").normalize).to eq("münchen")
+      end
+
+      it "strips underscores" do
+        expect(Tag.new("tag_string").normalize).to eq("tagstring")
+      end
+
+      it "strips punctuation" do
+        expect(Tag.new("tag,;@!'\"?.string").normalize).to eq("tagstring")
+      end
+    end
+  end
+end
