fc239935ca6e8c75b3bfe50a06a73bdb366b2349
Doug Emery <emeryr@upenn.edu>
2016-02-26 10:52:38 -0500

    Add evidence specs

--- spec/models/evidence_spec.rb
+++ spec/models/evidence_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Evidence, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'factories' do
+    it "creates an evidence object" do
+      expect(create(:evidence)).to be_a Evidence
+    end
+
+    it "creates evidence that's on flickr" do
+      expect(create(:evidence_on_flickr)).to be_a Evidence
+    end
+  end
+
+  context 'flickr_metadata' do
+    it_behaves_like 'flickr_metadata'
+
+  end
 end
