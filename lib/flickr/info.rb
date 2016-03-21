203ed5d42d08730df5ec4dfc2f85d1bf4f966820
Doug Emery <emeryr@upenn.edu>
2016-03-21 09:54:26 -0400

    Generate short Flickr URL

--- lib/flickr/info.rb
+++ lib/flickr/info.rb
@@ -36,6 +36,23 @@ module Flickr
       "#{photostream_url}/#{photo_id}"
     end
 
+    SHORT_URL = 'https://flic.kr/p/'.freeze
+    ##
+    # Generate the short URL for Flickr. The `url_mod` can be one of:
+    #
+    #     `nil`   Return short URL for photo page [default]
+    #     `"m"`   Small
+    #     `"s"`   Square
+    #     `"t"`   Thumbnail
+    #     `"q"`   Large square
+    #     `"n"`   Small 320
+    def short_url url_mod=nil
+      return nil if photo_id.blank?
+
+      mod_string = url_mod.nil? ? '' : "_#{url_mod}.jpg"
+      SHORT_URL + base58(photo_id) + mod_string
+    end
+
     def photo_id
       @info['id']
     end
@@ -46,6 +63,33 @@ module Flickr
 
     def owner_data
       @info and @info['owner']
+    end
+
+    private
+    # base58 code below taken directly from FlickRaw. Here:
+    #
+    # <https://github.com/hanklords/flickraw/blob/master/lib/flickraw/api.rb>
+    #
+    # FlickRaw has a method for creating these URLs, but it depends on the
+    # FlickRaw Response object, which I don't store, don't want to recreate or
+    # depend on.
+    #
+    # There is another way to handle this: The info hash could be wrapped in a
+    # class that uses method missing to extract hash values and pass that
+    # wrapper to FlickRaw. This would probably work. This class could also
+    # implement method missing and be handed to FlickRaw.
+    BASE58_ALPHABET="123456789abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ".freeze
+    def base58(id)
+      id = id.to_i
+      alphabet = BASE58_ALPHABET.split(//)
+      base = alphabet.length
+      puts id
+      begin
+        id, m = id.divmod(base)
+        r = alphabet[m] + (r || '')
+        puts "#{r} <= #{id}, #{m}"
+      end while id > 0
+      r
     end
   end
 end
