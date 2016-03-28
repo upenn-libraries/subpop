0b7d1f8377a27d8ee5f27639d1f0dac98c9f0398
Doug Emery <emeryr@upenn.edu>
2016-03-28 16:01:53 -0400

    Buttons to un/publish items on book show

--- app/models/title_page.rb
+++ app/models/title_page.rb
@@ -4,4 +4,14 @@ class TitlePage < ActiveRecord::Base
   belongs_to :book
   belongs_to :photo
 
+  delegate :full_name, to: :book, prefix: true, allow_nil: true
+
+  def name
+    return "title page" unless book_full_name.present?
+    "title page of #{book_full_name}"
+  end
+
+  def to_s
+    model_name.human
+  end
 end
