9306b40a92b2fab50a22db2aac69538ab1b2c37f
Doug Emery <emeryr@upenn.edu>
2016-04-12 11:19:27 -0400

    Fix stub evidence request spec

--- spec/requests/evidence_spec.rb
+++ spec/requests/evidence_spec.rb
@@ -1,9 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe "Evidence", type: :request do
+  subject(:subject) { create(:evidence) }
+
   describe "GET /evidence" do
     it "works! (now write some real specs)" do
-      get evidence_index_path
+      get @evidence
       expect(response).to have_http_status(200)
     end
   end
