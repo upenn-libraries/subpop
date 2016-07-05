5359ec68152973a00ff0942dd375cb64572bad93
Doug Emery <emeryr@upenn.edu>
2016-07-05 14:44:07 -0400

    Don't allow users to self-register.

--- config/routes.rb
+++ config/routes.rb
@@ -39,7 +39,15 @@ Rails.application.routes.draw do
   devise_scope :user do
     get '/login' => 'devise/sessions#new'
     get '/logout' => 'devise/sessions#destroy'
+    get 'users/edit' => 'users/registrations#edit', as: 'edit_user_registration'
+    patch 'users' => 'users/registrations#update', as: 'user_registration'
   end
+
+  # devise_for :users, :skip => [:registrations]
+  # as :user do
+  #   get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
+  #   put 'users' => 'devise/registrations#update', :as => 'user_registration'
+  # end
 
   resources :user, :controller => "user"
 
