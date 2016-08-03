c4fdc70f0a7bb8c25c0124b78a020182c84ced95
Doug Emery <emeryr@upenn.edu>
2016-08-02 21:41:37 -0400

    Use daemons for delayed_job

--- config/initializers/delayed_job.rb
+++ config/initializers/delayed_job.rb
@@ -1,3 +1,4 @@
 Delayed::Worker.sleep_delay = 5
 Delayed::Worker.max_attempts = 1
 Delayed::Worker.destroy_failed_jobs = false
+Delayed::Worker.logger = Logger.new(File.join(Rails.root, 'log', 'delayed_job.log'))
