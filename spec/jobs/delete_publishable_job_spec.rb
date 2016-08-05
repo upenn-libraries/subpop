bc765cd442a5c3f7525cdda78a874153b0fe9f43
Doug Emery <emeryr@upenn.edu>
2016-08-05 11:56:17 -0400

    Fill in DeletePublishableJob spec

--- spec/jobs/delete_publishable_job_spec.rb
+++ spec/jobs/delete_publishable_job_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe DeletePublishableJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:evidence) { create(:evidence) }
+
+  it 'enquues the job' do
+    ActiveJob::Base.queue_adapter = :test
+    expect {
+      DeletePublishableJob.perform_later evidence
+    }.to have_enqueued_job(DeletePublishableJob).with evidence
+  end
+
+  it 'executes destroy' do
+    expect(evidence).to receive(:destroy)
+    described_class.perform_now evidence
+  end
+
 end
