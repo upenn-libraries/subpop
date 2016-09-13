f602b7f363c59624c29ddc2bc9ea6c6ed9ccfc0e
Doug Emery <emeryr@upenn.edu>
2016-09-13 13:19:55 -0400

    Have photo abilities determined by associations.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -24,7 +24,23 @@ class Ability
     can :manage, Book,      created_by_id: user.id
     can :manage, Evidence,  book: { created_by_id: user.id }
     can :manage, TitlePage, book: { created_by_id: user.id }
-    can :manage, Photo,     book: { created_by_id: user.id }
+
+    # Have photo abilities depend on associated objects.
+    #
+    # https://github.com/CanCanCommunity/cancancan/wiki/Share-Ability-Definitions
+    can :manage, Photo do |photo|
+      case
+      when photo.book.present?
+        can? :manage, photo.book
+      when photo.evidence.present?
+        can? :manage, photo.evidence.first
+      when photo.title_pages.present?
+        can? :manage, photo.title_pages.first
+      when photo.context_images.present?
+        can? :manage, context_images.first
+      end
+    end
+
     can :manage, Name,      created_by_id: user.id
   end
 end
