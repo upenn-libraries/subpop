9db3da6cf35d0abab4c7e3384a3ba091e2c1533f
Laura Aydelotte <aydel@upenn.edu>
2016-10-15 12:41:17 -0400

    fix 'remove title page' problem

--- app/controllers/title_pages_controller.rb
+++ app/controllers/title_pages_controller.rb
@@ -22,6 +22,7 @@ class TitlePagesController < ApplicationController
 
     respond_to do |format|
       if @title_page.save_by current_user
+
         format.html { redirect_to @book, notice: 'Added title page.' }
         format.json { render :show, status: :ok, location: @book }
       else
