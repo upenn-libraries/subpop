aa7f851069549b075fdae4123252ab0dd3ac54e7
Laura Aydelotte <aydel@upenn.edu>
2017-04-30 21:50:15 -0400

    adding gender to form

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+ActiveRecord::Schema.define(version: 20170411205038) do
 
   create_table "bookmarks", force: :cascade do |t|
     t.integer  "user_id",       limit: 4,     null: false
