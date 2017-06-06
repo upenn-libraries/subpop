535dc629ef683ee17c5d905ce3bd73f72b4cf848
Doug Emery <emeryr@upenn.edu>
2017-06-06 16:03:12 -0400

    Fix remediation view pages.

--- app/controllers/remediations_controller.rb
+++ app/controllers/remediations_controller.rb
@@ -17,17 +17,13 @@ class RemediationsController < ApplicationController
     @remediation = Remediation.new
   end
 
-  # GET /remediations/1/edit
-  def edit
-  end
-
   # POST /remediations
   # POST /remediations.json
   def create
-    @remediation = Remediation.new(remediation_params)
+    @remediation = Remediation.new remediation_params
 
     respond_to do |format|
-      if @remediation.save
+      if @remediation.save_by current_user
         format.html { redirect_to @remediation, notice: 'Remediation was successfully created.' }
         format.json { render :show, status: :created, location: @remediation }
       else
@@ -37,38 +33,14 @@ class RemediationsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /remediations/1
-  # PATCH/PUT /remediations/1.json
-  def update
-    respond_to do |format|
-      if @remediation.update(remediation_params)
-        format.html { redirect_to @remediation, notice: 'Remediation was successfully updated.' }
-        format.json { render :show, status: :ok, location: @remediation }
-      else
-        format.html { render :edit }
-        format.json { render json: @remediation.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /remediations/1
-  # DELETE /remediations/1.json
-  def destroy
-    @remediation.destroy
-    respond_to do |format|
-      format.html { redirect_to remediations_url, notice: 'Remediation was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_remediation
-      @remediation = Remediation.find(params[:id])
+      @remediation = Remediation.find params[:id]
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def remediation_params
-      params.require(:remediation).permit(:problems, :created_by_id, :updated_by_id)
+      params.require(:remediation).permit(:spreadsheet)
     end
 end
