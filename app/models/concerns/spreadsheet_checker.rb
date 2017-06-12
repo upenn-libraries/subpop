21abf1c4f5ab5335744487bd0e5cb4406ceeaee1
George Murnaghan Gordon <gordonmgeorge@gmail.com>
2017-06-12 11:22:02 -0400

    fill-out spreadsheet checking structure

--- app/models/concerns/spreadsheet_checker.rb
+++ app/models/concerns/spreadsheet_checker.rb
@@ -13,4 +13,25 @@ module SpreadsheetChecker
     check
     self.problems.blank?
   end
+
+  # methods to be defined in body of SpreadSheetColumn class
+  # -------------------------------------------------------------------
+  def check_year field_name, value, required=false
+    return [] if required == false && value.blank?
+    return [field_name, "is required"] if value.blank?
+    return [field_name, "is not a valid year"] unless valid_year? value
+    [] 
+  end
+
+  def valid_year? value
+    return false unless value.to_s.strip.to_i.to_s == value.to_s.strip
+    (-5000..3000).include? value.to_i
+  end
+  # ------------------------------------------------------------------
+
 end
+
+
+# class SpreadSheetColumn (seperate file in concerns folder)
+# @column_number
+# initialize column objects in batches of ~30 ?
--- spec/models/remediation_spec.rb
+++ spec/models/remediation_spec.rb
@@ -54,6 +54,87 @@ RSpec.describe Remediation, type: :model do
       expect(create :remediation).to be_problem_free
     end
 
-    it 'has a format heading'
+    it 'has required headings'
+
+    it 'is valid if non-provenance is true and format is blank'
+
+    it 'is invalid if non-provenance is true and format is not blank'
+
+    context 'required for provenance' do
+      it 'is valid if image is present'
+
+      it 'is invalid if imagine is blank'
+
+      it 'is valid if current repository is present'
+
+      it 'is invalid if current repository is blank'
+
+      it 'is valid if call number is present'
+
+      it 'is invalid if call number is blank'
+
+      it 'is valid if title is present'
+
+      it 'is invalid if title is blank'
+
+      it 'is valid if format is other and format_other is present'
+
+      it 'is invalid if format is other and format_other is blank'
+    end
+
+    context 'field value' do
+      it 'is valid if format is in the format list'
+
+      it 'is invalid if format is not in the format list'
+
+      it 'is valid if location-in-book is in the location-in-book list'
+
+      it 'is invalid if location-in-book is not in the location-in-book list'
+
+      it 'is valid if content type is in the content type list'
+
+      it 'is invalid if content type is not in the content type list'
+
+      # for 'piped' entries
+      it 'is valid if all content types are in the content type list' 
+      
+      it 'is invalid if not all content types are in the content type list' 
+      # ------------------
+
+      it 'is valid if date of publication is an integer year'
+
+      it 'is valid if evidence: date start is an integer year'
+      
+      it 'is valid if evidence: date end is an integer year'
+
+      it 'is valid if evidence: date is an integer year'
+
+      it 'is invalid if date of publication is not a valid year'
+
+      it 'is invalid if evidence: date start is not a valid year'
+      
+      it 'is invalid if evidence: date end is not a valid year'
+
+      it 'is invalid if evidence: date is not a valid year'
+
+      it 'is valid if date of publication is blank'
+
+      it 'is valid if evidence: date start is blank'
+      
+      it 'is valid if evidence: date end is blank'
+
+      it 'is valid if evidence: date is blank'
+    end
+
+    context 'images' do
+      it 'is valid if flick_url is live'
+
+      it 'is invalid if flick_url is not live'
+
+      it 'is valid if image file can be found on OPenn'
+
+      it 'is invalid if image file cannot be found on OPenn'
+    end
+
   end
 end
