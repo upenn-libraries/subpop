15abeca0c0aac29d12d8ba1b386c346e46a49e54
Doug Emery <emeryr@upenn.edu>
2017-06-19 12:06:31 -0400

    Remove pry statements

--- app/jobs/spreadsheet_remediation_job.rb
+++ app/jobs/spreadsheet_remediation_job.rb
@@ -2,9 +2,7 @@ class SpreadsheetRemediationJob < ActiveJob::Base
   queue_as :default
 
   def perform remediation_agent
-    binding.pry
     remediation_agent.remediate
     remediation_agent.save!
-    binding.pry
   end
 end
