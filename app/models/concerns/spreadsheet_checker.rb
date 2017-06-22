8c88a46fb9aad4dd61f2ae5df036b53f9b13522f
George Murnaghan Gordon <gordonmgeorge@gmail.com>
2017-06-22 10:34:55 -0400

    fix spreadsheet-checking code to pass tests

--- app/models/concerns/spreadsheet_checker.rb
+++ app/models/concerns/spreadsheet_checker.rb
@@ -5,93 +5,75 @@
 module SpreadsheetChecker
   extend ActiveSupport::Concern
 
- def problem_free?
+  def problem_free?
    check
    self.problems.blank?
- end 
+  end
 
- def check
-   self.problems = []
-   verify_headings
-   return if self.problems.present?  
-   spreadsheet_data.each do |entry| # how to invoke?
-     heading = entry[:column]
-     entry_problems = check_entry(entry)
-     self.problems << [heading, entry_problems] unless entry_problems.blank?
-   end
- end
-
+  def check
+   self.problems = {}
+   verify_headings(spreadsheet)
+   return if self.problems.present?
+   spreadsheet.each{ |entry| check_entry(entry) } # spreadsheet_data
+   true
+  end
 
   def verify_headings sheet
+    self.problems[:headings] ||= []
+    headings.each do |heading|
+      self.problems[:headings] << "ERROR--invalid heading #{heading}" unless VALID_FIELD_NAMES.include?(heading)
+    end
     REQUIRED_FIELDS.each do |field|
-      self.problems << "missing required field #{field}" unless headings[field]
+      self.problems[:headings] << "ERROR--missing required field #{field}" unless headings[field]
     end
   end
-
-  # def normalize_heading heading
-  #   heading.strip.gsub(/[^[:alnum:]_]+/, '')
-  # end
 
   def check_entry entry_hash
-    return ['no photo'] unless check_for_photo(entry_hash) 
-    return check_non_provenance(entry_hash) if entry_hash[:evidence_format].blank?
-
-    problems = entry_hash.keys.inject([]) do |list, field|
-      field_problems = check_field(field entry[field) 
-      list << field_problems unless field_problems.blank?
-    end
+    self.problems ||= {}
+    return unless entry_hash[:evidence_format]
+    @col_sym = entry_hash[:column].to_sym
+    self.problems[@col_sym] ||= []
+    check_photo(entry_hash[:flickr_url])
+    FIELDS_TO_CHECK.each{ |f| check_field(f, entry_hash[f]) }
     @other_format_flag = false
-    problems
   end
 
-  def check_photo entry
-    path_prescence = %i(flickr_url file_name).map{ |f| entry[f].blank? }
-    return false unless path_prescence.compact = [true]
-    path_location = path_prescence[0] ? 0 : 1 
-    path = %i(flickr_url file_name).map{ entry[f] }[path_location]
-    path_location == 0 ? check_flickr_photo(path) : check_other_photo(path)
-  end
-
-  def check_flickr_photo path
-  end
-
-  def check_other_photo path
-  end
-
-  def check_non_provenance entry
+  def check_photo path
+    unless :no_problem
+      self.add_problem(:flickr_url, 'photo problem')
+    end
   end
 
   def check_field(field_name, value)
     required_field = REQUIRED_FIELDS.include?(field_name)
-    required_field = true if field_name == other_format && @other_format_flag
-    @other_format_flag = true if field_name == :evidence_other_format
+    required_field = true if field_name == :evidence_other_format && @other_format_flag
+    @other_format_flag = true if field_name == :evidence_format && (value == 'Other Format')
 
-    return [field_name, 'is required'] if value.blank? && required_field
-    return [] if value.blank? 
-    # if not blank
+    return add_problem(field_name, 'is required') if value.blank? && required_field
+    return if value.blank?
     return check_list_field(field_name, value) if LIST_FIELDS.include?(field_name)
     return check_year_field(field_name, value) if YEAR_FIELDS.include?(field_name)
-    []
+    nil
   end
 
   def check_list_field field_name, value
     return check_piped_content_type(value) if field_name == :evidence_content_type && value.include?('|')
-    return [] if VALID_LIST_VALUES[field_name].include? value
-    [field_name, 'has invalid data entered']
+    return if VALID_LIST_VALUES[field_name].include? value
+    add_problem(field_name, 'has invalid data entered')
   end
 
   def check_piped_content_type value
     types = value.split '|'
-    no_problems = types.all? do |t| 
-      VALID_LIST_VALUES[:evidence_content_type].include? t.strip 
+    no_problems = types.all? do |t|
+      VALID_LIST_VALUES[:evidence_content_type].include? t.strip
     end
-    return [] if no_problems
-    [:evidence_content_type, 'has invalid data entered']
+    return if no_problems
+    add_problem(:evidence_content_type, 'has invalid data entered')
   end
 
   def check_year_field field_name, value
-    return [] if valid_year?(value)
-    [field_name, 'is not a valid year']
+    return if valid_year?(value)
+    add_problem(field_name, 'is not a valid year')
   end
 
   def valid_year? value
@@ -99,10 +81,76 @@ module SpreadsheetChecker
     (-5000..3000).include? value.to_i
   end
 
+  def add_problem field_name, message
+    self.problems ||= {}
+    s = format_message(field_name, message)
+    (self.problems[@col_sym] ||= []) << s
+  end
+
+  def format_message field_name, message
+    "ERROR--[#{field_name}]: #{message}"
+  end
+
+  def assemble_message
+    message = ''
+    self.problems.keys.sort.each{ |k| message << "#{k} problems[k] \n\n" }
+    message
+  end
+
+
+# VALID_FIELD_NAMES = %i(
+# # url??
+  VALID_FIELD_NAMES = %i(
+    copy_current_repository
+    copy_current_collection
+    copy_current_owner
+    copy_current_geographic_location
+    copy_call_number_shelf_mark
+    copy_volume_number
+    copy_other_id
+    copy_author
+    copy_title
+    copy_place_of_publication
+    copy_date_narrative
+    copy_date_of_publication
+    copy_printer_publisher_scribe
+    evidence_location_in_book
+    evidence_format
+    evidence_other_format
+    evidence_content_type
+    evidence_transcription
+    evidence_translation
+    evidence_date_start
+    evidence_date_end
+    evidence_date
+    evidence_place
+    evidence_citation
+    evidence_comments
+    id_owner
+    id_librarian
+    id_bookseller_auction_house
+    id_binder
+    id_annotator
+    id_unknown_role
+    problem_stuff
+  )
+
+  FIELDS_TO_CHECK = %i(
+    copy_current_repository
+    copy_title
+    copy_call_number_shelf_mark
+    copy_date_of_publication
+    evidence_format
+    evidence_other_format
+    evidence_content_type
+    evidence_date
+    evidence_date_start
+    evidence_date_end
+  )
 
   REQUIRED_FIELDS = %i(
     copy_current_repository
-    copy_title 
+    copy_title
     copy_call_number_shelf_mark
     evidence_format
     )
--- spec/models/remediation_spec.rb
+++ spec/models/remediation_spec.rb
@@ -1,50 +1,5 @@
 require 'rails_helper'
 
-let(:nonprovenance_item) {
-  {
-    column: 'C',
-    image_url: 'image.url',
-    not_provenance: 'x'
-  }
-}
-
-let(:valid_bookplate) {
-  {
-    column:                           "C",
-    flick_url:                        "https://www.flickr.com/photos/58558794@N07/6106275763",
-    url_to_catalog:                   "http://franklin.library.upenn.edu/record.html?id=FRANKLIN_5050597",
-    copy_current_repository:          "Penn Libraries",
-    copy_current_collection:          "American Culture Class Collection",
-    copy_current_geographic_location: "Philadelphia",
-    copy_call_number_shelf_mark:      "AC8 H3188 A825y",
-    copy_title:                       "Youth's keepsake : A Christmas and New Year's gift for young people.",
-    copy_place_of_publication:        "United States Massachusetts Boston.",
-    copy_date_of_publication:         "1835",
-    evidence_format:                  "Bookplate/Label",
-    evidence_comments:                "Bookplate of Carroll Atwood Wilson (1886-1947), American lawyer (he served as chief legal counsel to the Guggenheims) and book collector.",
-    id_owner:                         "Wilson, Carroll A. (Carroll Atwood), 1886-1947"
-  }
-}
-
-
-let(:minimal_bookplate) {
-  {
-    column:                           "C",
-    flick_url:                        "https://www.flickr.com/photos/58558794@N07/6106275763",
-    copy_call_number_shelf_mark:      "AC8 H3188 A825y",
-    copy_title:                       "Youth's keepsake : A Christmas and New Year's gift for young people.",
-    evidence_format:                  "Bookplate/Label",
-    
-  }
-}
-
-
-let(:insufficient_bookplate) {
-  {
-
-  }
-}
-
 RSpec.describe Remediation, type: :model do
   let(:subject) {  create(:remediation) }
   let(:valid_attributes) {
@@ -52,6 +7,144 @@ RSpec.describe Remediation, type: :model do
       spreadsheet: open("#{Rails.root}/spec/fixtures/spreadsheets/Valid_Flickr_sheet.xlsx")
     }
   }
+
+  let(:nonprovenance_item) {
+    {
+      column: 'C',
+      image_url: 'image.url',
+      not_provenance: 'x',
+      evidence_format: ''
+    }
+  }
+
+  let(:valid_bookplate) {
+    {
+      column:                           "C",
+      flick_url:                        "https://www.flickr.com/photos/58558794@N07/6106275763",
+      url_to_catalog:                   "http://franklin.library.upenn.edu/record.html?id=FRANKLIN_5050597",
+      copy_current_repository:          "Penn Libraries",
+      copy_current_collection:          "American Culture Class Collection",
+      copy_current_geographic_location: "Philadelphia",
+      copy_call_number_shelf_mark:      "AC8 H3188 A825y",
+      copy_title:                       "Youth's keepsake : A Christmas and New Year's gift for young people.",
+      copy_place_of_publication:        "United States Massachusetts Boston.",
+      copy_date_of_publication:         "1835",
+      evidence_format:                  "Bookplate/Label",
+      evidence_comments:                "Bookplate of Carroll Atwood Wilson (1886-1947), American lawyer (he served as chief legal counsel to the Guggenheims) and book collector.",
+      id_owner:                         "Wilson, Carroll A. (Carroll Atwood), 1886-1947"
+    }
+  }
+
+
+  let(:minimal_bookplate) {
+    {
+      column:                           "MB",
+      flick_url:                        "https://www.flickr.com/photos/58558794@N07/6106275763",
+      copy_current_repository:          "Penn",
+      copy_call_number_shelf_mark:      "AC8 H3188 A825y",
+      copy_title:                       "Youth's keepsake : A Christmas and New Year's gift for young people.",
+      evidence_format:                  "Bookplate/Label",
+
+    }
+  }
+
+  # let(:evidence_date_start_invalid) {
+  #   {
+  #     column:                           "C",
+  #     flick_url:                        "https://www.flickr.com/photos/58558794@N07/6106275763",
+  #     url_to_catalog:                   "http://franklin.library.upenn.edu/record.html?id=FRANKLIN_5050597",
+  #     copy_current_repository:          "Penn Libraries",
+  #     copy_current_collection:          "American Culture Class Collection",
+  #     copy_current_geographic_location: "Philadelphia",
+  #     copy_call_number_shelf_mark:      "AC8 H3188 A825y",
+  #     copy_title:                       "Youth's keepsake : A Christmas and New Year's gift for young people.",
+  #     copy_place_of_publication:        "United States Massachusetts Boston.",
+  #     copy_date_of_publication:         "1835",
+  #     evidence_format:                  "Bookplate/Label",
+  #     evidence_comments:                "Bookplate of Carroll Atwood Wilson (1886-1947), American lawyer (he served as chief legal counsel to the Guggenheims) and book collector.",
+  #     evidence_date_start:              "c. 1920s",
+  #     id_owner:                         "Doug"
+  #   }
+  # }
+
+ ##############################################
+
+  let(:all_required_fields) {
+    {
+      column:                      'RQ',
+      flick_url:                   'flickr.url',
+      copy_current_repository:     'Required Repositories',
+      copy_title:                  'required title',
+      copy_call_number_shelf_mark: 'required0.1 number2',
+      evidence_format:             'Inscription'
+    }
+  }
+
+ let(:missing_required_fields) {
+    {
+      column:                      'MQ',
+      flick_url:                   'flickr.url',
+      copy_current_repository:     '',
+      copy_title:                  '',
+      copy_call_number_shelf_mark: '',
+      evidence_format:             'Inscription',
+      copy_place_of_publication:   'no place',
+      evidence_comments:           'no_comment'
+    }
+  }
+
+  let(:all_good_dates) {
+    {
+      column:                    'AD',
+      flick_url:                 'flickr.url',
+      copy_date_of_publication:  '1111',
+      evidence_date_start:       '1112',
+      evidence_date_end:         '1113',
+      evidence_date:             '1114',
+      evidence_format:           'Inscription'
+    }
+  }
+
+
+  let(:all_bad_dates) {
+    {
+      column:                    'BD',
+      flick_url:                 'flickr.url',
+      copy_date_of_publication:  '8111',
+      evidence_date_start:       'a112',
+      evidence_date_end:         'c.1113',
+      evidence_date:             '1114-3',
+      evidence_format:           'Inscription'
+    }
+  }
+
+   let(:bad_format_bad_content_type) {
+    {
+      column:                'BF',
+      flick_url:             'flickr.url',
+      evidence_format:       'an invalid format',
+      evidence_content_type: 'an invalid content_type | Armorial'
+    }
+  }
+
+   let(:good_other_format) {
+    {
+      column:                'GO',
+      flick_url:             'flickr.url',
+      evidence_format:       'Other Format',
+      evidence_other_format: 'pressed flower'
+    }
+  }
+
+   let(:bad_other_format) {
+    {
+      column:                'BO',
+      flick_url:             'flickr.url',
+      evidence_format:       'Other Format',
+      evidence_other_format: ''
+    }
+  }
+
   context 'factory' do
     it 'creates a Remediation' do
       expect(create :remediation).to be_a Remediation
@@ -99,76 +192,178 @@ RSpec.describe Remediation, type: :model do
       expect(create :remediation).to be_problem_free
     end
 
-    it 'has required headings'
 
-    it 'is valid if non-provenance is true and format is blank'
+    it 'has no invalid headings' do
+    # subject. # # # ?
+    # expect(subject.problems[:headings].join).to_not include('invalid heading')
+    end
 
-    it 'is invalid if non-provenance is true and format is not blank'
+    it 'has all required headings' do
+    # subject. # # # ?
+    # expect(subject.problems[:headings].join).to_not include('missing required')
+    end
+
+
 
     context 'required for provenance' do
       it 'is valid if image is present'
 
       it 'is invalid if image is blank'
 
-      it 'is valid if current repository is present'
+      it 'is valid if current repository is present' do
+        subject.check_entry(all_required_fields)
+        expect(subject.problems[:RQ].join).to_not include('copy_current_repository')
+      end
 
-      it 'is invalid if current repository is blank'
+      it 'is invalid if current repository is blank' do
+        subject.check_entry(missing_required_fields)
+        expect(subject.problems[:MQ].join).to include('copy_current_repository')
+      end
 
-      it 'is valid if call number is present'
+      it 'is valid if call number is present' do
+        subject.check_entry(all_required_fields)
+        expect(subject.problems[:RQ].join).to_not include('copy_call_number_shelf_mark')
+      end
 
-      it 'is invalid if call number is blank' 
+      it 'is invalid if call number is blank' do
+        subject.check_entry(missing_required_fields)
+        expect(subject.problems[:MQ].join).to include('copy_call_number_shelf_mark')
+      end
 
-      it 'is valid if title is present'
+      it 'is valid if title is present' do
+        subject.check_entry(all_required_fields)
+        expect(subject.problems[:RQ].join).to_not include('copy_title')
+      end
 
-      it 'is invalid if title is blank'
+      it 'is invalid if title is blank' do
+        subject.check_entry(missing_required_fields)
+        expect(subject.problems[:MQ].join).to include('copy_title')
+      end
 
-      it 'is valid if format is other and format_other is present'
+      it 'is valid if format is other and other_format is present' do
+        subject.check_entry(good_other_format)
+        expect(subject.problems[:GO].join).to_not include('evidence_other_format')
+      end
 
-      it 'is invalid if format is other and format_other is blank'
+      it 'is invalid if format is other and other_format is blank' do
+        subject.check_entry(bad_other_format)
+        expect(subject.problems[:BO].join).to include('evidence_other_format')
+      end
     end
 
     context 'field value' do
-      it 'is valid if format is in the format list'
+      it 'is valid if format is in the format list' do
+        valid_formats = [
+          "Binding",
+          "Binding Waste",
+          "Bookplate/Label",
+          "Drawing/Illumination",
+          "Inscription",
+          "Other paste-in",
+          "Seal",
+          "Stamp -- inked",
+          "Stamp -- blind or embossed",
+          "Stamp -- perforated",
+          "Other Format",
+          "Title Page (non-evidence)",
+          "Context Image (non-evidence"
+        ]
+        single_field_hashes = valid_formats.map{ |fmt| {column: 'A', flick_url: 'flickr.url', evidence_format: fmt} }
+        single_field_hashes.each{ |h| subject.check_entry(h) }
+        expect(subject.problems[:A].join).to_not include('evidence_format')
+      end
 
-      it 'is invalid if format is not in the format list'
+      it 'is invalid if format is not in the format list' do
+        subject.check_entry(bad_format_bad_content_type)
+        expect(subject.problems[:BF].join).to include('evidence_format')
+      end
 
-      # it 'is valid if location-in-book is in the location-in-book list'
+      it 'is valid if all content types are in the content type list' do
+        valid_content_types = [
+          "Armorial",
+          "Bibliographic Note",
+          "Binder's Mark",
+          "(De)Accession Mark",
+          "Effaced",
+          "Forgery/Copy",
+          "Gift",
+          "Monogram",
+          "Price/Purchase Information",
+          "Sale Record",
+          "Shelf Mark",
+          "Seller's Mark",
+          "Signature"
+        ]
+        single_field_hashes = valid_content_types.map{ |c_t| {column: 'A', evidence_format: 'Inscription', evidence_content_type: c_t} }
+        single_field_hashes.each{ |h| subject.check_entry(h) }
+        subject.check_entry({column: 'A', flick_url: 'flickr.url', evidence_format: 'Inscription', evidence_content_type: 'Armorial | Gift | Monogram'})
+        expect(subject.problems[:A].join).to_not include('evidence_content_type')
+      end
 
-      it 'is invalid if location-in-book is not in the location-in-book list'
+      it 'is invalid if not all content types are in the content type list' do
+        subject.check_entry(bad_format_bad_content_type)
+        expect(subject.problems[:BF].join).to include('evidence_content_type')
+      end
 
-      it 'is valid if content type is in the content type list'
+      it 'is valid if date of publication is an integer year' do
+        subject.check_entry(all_good_dates)
+        expect(subject.problems[:AD].join).to_not include('copy_date_of_publication')
+      end
 
-      it 'is invalid if content type is not in the content type list'
+      it 'is valid if evidence: date start is an integer year' do
+        subject.check_entry(all_good_dates)
+        expect(subject.problems[:AD].join).to_not include('evidence_date_start')
+      end
 
-      # for 'piped' entries
-      it 'is valid if all content types are in the content type list' 
-      
-      it 'is invalid if not all content types are in the content type list' 
-      # ------------------
+      it 'is valid if evidence: date end is an integer year' do
+        subject.check_entry(all_good_dates)
+        expect(subject.problems[:AD].join).to_not include('evidence_date_end')
+      end
 
-      it 'is valid if date of publication is an integer year'
+      it 'is valid if evidence: date is an integer year' do
+        subject.check_entry(all_good_dates)
+        expect(subject.problems[:AD].join).to_not include('evidence_date]')
+      end
 
-      it 'is valid if evidence: date start is an integer year'
-      
-      it 'is valid if evidence: date end is an integer year'
+      it 'is invalid if date of publication is not a valid year' do
+        subject.check_entry(all_bad_dates)
+        expect(subject.problems[:BD].join).to include('copy_date_of_publication')
+      end
 
-      it 'is valid if evidence: date is an integer year'
+      it 'is invalid if evidence: date start is not a valid year' do
+        subject.check_entry(all_bad_dates)
+        expect(subject.problems[:BD].join).to include('evidence_date_start')
+      end
 
-      it 'is invalid if date of publication is not a valid year'
+      it 'is invalid if evidence: date end is not a valid year' do
+        subject.check_entry(all_bad_dates)
+        expect(subject.problems[:BD].join).to include('evidence_date_end')
+      end
 
-      it 'is invalid if evidence: date start is not a valid year'
-      
-      it 'is invalid if evidence: date end is not a valid year'
+      it 'is invalid if evidence: date is not a valid year' do
+        subject.check_entry(all_bad_dates)
+        expect(subject.problems[:BD].join).to include('evidence_date]')
+      end
 
-      it 'is invalid if evidence: date is not a valid year'
+      it 'is valid if date of publication is blank' do
+        subject.check_entry(minimal_bookplate)
+        expect(subject.problems[:MB].join).to_not include('copy_date_of_publication')
+      end
 
-      it 'is valid if date of publication is blank'
+      it 'is valid if evidence: date start is blank' do
+        subject.check_entry(minimal_bookplate)
+        expect(subject.problems[:MB].join).to_not include('evidence_date_start')
+      end
 
-      it 'is valid if evidence: date start is blank'
-      
-      it 'is valid if evidence: date end is blank'
+      it 'is valid if evidence: date end is blank' do
+        subject.check_entry(minimal_bookplate)
+        expect(subject.problems[:MB].join).to_not include('evidence_date_end')
+      end
 
-      it 'is valid if evidence: date is blank'
+      it 'is valid if evidence: date is blank' do
+        subject.check_entry(minimal_bookplate)
+        expect(subject.problems[:MB].join).to_not include('evidence_date]')
+      end
     end
 
     context 'images' do
@@ -180,7 +375,7 @@ RSpec.describe Remediation, type: :model do
 
       it 'is invalid if image file cannot be found on OPenn'
     end
-
+  end
   context 'spreadsheet extractor' do
     it_behaves_like 'spreadsheet_extractor'
   end
